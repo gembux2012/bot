b09230b8053e5f1148c86801b02d0a48b36485b4
alex <gembux2012@gmail.com>
2021-02-01 17:53:39 +0700

    current save

--- app/Main.hs
+++ app/Main.hs
@@ -1,91 +1,85 @@
+{-# LANGUAGE BlockArguments        #-}
 {-# LANGUAGE DeriveGeneric         #-}
 {-# LANGUAGE DerivingStrategies    #-}
+{-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
-
 {-# LANGUAGE OverloadedStrings     #-}
-{-# LANGUAGE BlockArguments #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE RecordWildCards       #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
 
 module Main where
-import           Config               (Subd (..), Config (..), LogOpts (..),
-                                       readConfig1,  )
-import Exception
-import           Log                  (Log (..), Logger (..), Priority (..),
-                                       logLn, 
-                                        )
-import Data.Has (Has, getter,modifier)
-import           GHC.Generics         (Generic)
 
-import           Control.Monad.Reader (runReaderT)
-import Data.Text (unpack, pack, Text)
-import           Data.Time.Format     (defaultTimeLocale, formatTime)
-import           Data.Time.LocalTime  (getZonedTime)
-import           GHC.Exception        (prettyCallStack)
-import           GHC.Stack            (HasCallStack, callStack)
-import Control.Exception.Base (catch, SomeException, fromException, try)
-import Data.Maybe 
-
-
-
+import           Config                 (Config (..), LogOpts (..), Subd (..),
+                                         readConfig1)
+import           Control.Exception.Base (SomeException, catch, fromException,
+                                         try)
+import           Control.Monad.Reader   (runReaderT)
+import           Data.Has               (Has, getter, modifier)
+import           Data.Maybe
+import           Data.Text              (Text, append, pack, unpack)
+import           Data.Time.Format       (defaultTimeLocale, formatTime)
+import           Data.Time.LocalTime    (getZonedTime)
+import           Exception
+import           GHC.Exception          (prettyCallStack)
+import           GHC.Generics           (Generic)
+import           GHC.Stack              (HasCallStack, callStack)
+import           Log                    (Log (..), Logger (..), Priority (..),
+                                         logLn, printLog)
 
 {--
 app   =   Application
-                  { 
+                  {
                     logger = Logger   {
                                         dologLn =
                                         \LogOpts{..}  a -> do
                                         appendFile (show nameLogInfo) $  unpack a
                                         putStrLn $  unpack a
                                         putStrLn (prettyCallStack callStack)
-                    
+
                   }
                   }
 --}
-appendFile' path str  = catch (appendFile path  str) handler 
- where
-             handler :: SomeException -> IO ()
-             handler ex = putStrLn $ "Caught exception: " ++ show ex 
-newtype LogOpt
-  =LogOpt
-    { 
-      nameLog :: String
-    }      
-  
+appendFile' path str = catch (appendFile path str) handler
+  where
+    handler :: SomeException -> IO ()
+    handler ex = putStrLn $ "Caught exception: " ++ show ex
+
+newtype LogOpt = LogOpt
+  { nameLog :: String
+  }
+
 logOpt = LogOpt {nameLog = "log.log"}
-logX  str = appendFile  (nameLog logOpt) str
+
+logX str = appendFile (nameLog logOpt) str
+
 main :: IO ()
-main =do
-      conf <- readConfig1 
-      case conf of 
-        Left err -> putStrLn err
-        Right Config{..} ->do                                 --print $ pathToLog (logOpts)
-         let  app = Application
-               { 
-                 logger = 
-                     Logger {
-                              dologLn = \a -> do
-                                 result <- try (appendFile (pathToLog (logOpts) ++ nameLogInfo(logOpts))  $  unpack a):: IO (Either SomeException () ) 
-                                 case result of
-                                          Left ex  -> putStrLn $ "Caught exception: " ++ show ex
-                                          Right val -> return()
-                                  
-                                 --catch (appendFile (pathToLog (logOpts) ++ nameLogInfo(logOpts))  $  unpack a) handler 
-                        }
-               }
-         runReaderT (logLn $ pack "Hi") app
-                                 
-                 
+main = do
+  conf <- readConfig1
+  case conf of
+    Left err -> putStrLn err
+    Right Config {..} -> do
+      
+      let app = Application
+            { logger = Logger
+              { {---dologLn = \a -> do
+                  let appendFile' path str = try (appendFile path str) :: IO (Either SomeException ())
+                  result <- appendFile' (pathToLog (logOpts) ++ nameLogInfo (logOpts)) $ unpack a ++ "\n"
+                  case result of
+                   Left ex -> putStrLn $ "Caught exception: " ++ show ex
+                   Right val -> return () --}
+                   dologLn = \a -> printLog (pathToLog (logOpts))  (nameLogInfo (logOpts)) $ unpack a ++ "\n"  
+              }
+            }
+      --runReaderT (logLn $ pack "logger initialized") app
+      runReaderT (logLn $ pack "bot start") app
+
 
 data Application m = Application
- {
-  logger ::Logger m
- }
-   deriving stock (Generic)
+  { logger :: Logger m
+  }
+  deriving stock (Generic)
 
-
-instance Has (Logger m) (Application m ) where
- getter = logger
- modifier f a = a { logger = f . logger $ a }
- 
+instance Has (Logger m) (Application m) where
+  getter = logger
+  modifier f a = a {logger = f . logger $ a}
--- src/Log.hs
+++ src/Log.hs
@@ -5,21 +5,23 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module Log
   ( Loggable (..),
     Log (..),
     Logger (..),
     Priority (..),
-    logLn,
+    --logLn,
     
     --defaultLogOpts,
     SetOpts,
+    printLog
   )
 where
 
-import Config (Config, LogOpts (..), logOpts)
-import Control.Monad.Reader (ReaderT, lift)
+import Config (Config, LogOpts (..), logOpts, )
+import Control.Monad.Reader (ReaderT, lift, void)
 import Control.Monad.Reader.Class (asks)
 import Data.Has (Has (..))
 import Data.Text (Text, append, pack, unpack)
@@ -27,17 +29,19 @@ import Data.Time.Format (defaultTimeLocale, formatTime)
 import Data.Time.LocalTime (LocalTime, ZonedTime, getZonedTime)
 import GHC.Stack (HasCallStack)
 import Prelude
+import Control.Exception.Base (SomeException, try)
 
 class Loggable a where
   fromLoggable :: a -> Text
 
 class Monad m => Log m where
   logLn :: HasCallStack => Loggable a => a -> m ()
-  
+
 data Logger m = Logger
-  { dologLn :: HasCallStack => Text -> m ()
+  { 
+    dologLn :: HasCallStack => Text -> m ()
   }
-  
+
 
 --logLnInit ::  LogOpts ;
 
@@ -51,7 +55,7 @@ defaultLogOpts =
   LogOpts
     { pathToLog = "kjk",
       nameLogInfo = "bot.log"
-     
+
     }
 --}
 
@@ -65,10 +69,25 @@ instance
   Log (ReaderT r m)
   where
    logLn a =
-      asks getter >>= \(Logger doLog) -> lift . doLog . fromLoggable $ a
-      
+      asks getter >>= \(Logger  doLog) -> lift . doLog . fromLoggable $ a
+
 instance Loggable Text where
   fromLoggable = id
 
 --instance Loggable Priority where
 --  fromLoggable = pack . show
+
+printLogX :: [Char] -> [Char] -> String ->Int-> IO ()
+printLogX _ _ _ 0 = return() 
+printLogX path fName str n    =  do
+   result <- try (appendFile (path ++ fName) str )  :: IO (Either SomeException ())
+   case result of
+    Right val -> return ()
+    Left ex -> do 
+     if n == 2 then do  
+       printLogX "" fName (show ex <> ": log will be written to the current directory") (n-1)
+       else do
+        putStrLn (show ex <> ": cannot write a log, the app will be stopped") 
+        printLogX "" "" "" (n-1)
+        
+printLog path fName str = printLogX path fName str 2        
